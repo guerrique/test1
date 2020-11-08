11925853bcbefe6e0305a5305d427a1559bd7fb6
guerrique desmoulins guerrique@gmail.com <guerrique@gmail.com>
2020-11-08 18:21:20 +0800

    assignment answer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import UserInput from './User/UserInput';
+import UserOutput from './User/UserOutput';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    users: [
+      {name: 'Roberto'}
+    ]
+  }
+
+  changeNameHandler = (event) => {
+    this.setState({
+      users: [
+        { name: event.target.value }
+      ]
+    }
+    )
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <h1>First Assignment</h1>
+        <UserInput
+          name={this.state.users.[0].name}
+          input={this.changeNameHandler}/>
+        <UserOutput
+          name='Francis'/>
+        <UserOutput
+          name={this.state.users[0].name}/>
+
+      </div>
+
+    );
+  }
+
 }
 
 export default App;
---
+++ src/User/UserOutput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const userOutput = (props) => {
+  return (
+    <div>
+      <p>{props.name}: Le ciel est bleu.</p>
+      <p>Le soleil brille.</p>
+    </div>
+  )
+}
+
+export default userOutput;
